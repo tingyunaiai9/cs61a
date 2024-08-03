b069fb738451bcea459b6ba793ab4d136fba6a0b
tingyunaiai9 <2920825585@qq.com>
2024-08-03 17:39:53 +0800

    finish lab09
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,18 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        ((> num1 num2) 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  (lambda (x) (+ x num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+  (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (if (= n 1)
+      f
+      (composed f (repeat f (- n 1)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +24,7 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+  (if (zero? (modulo (max a b) (min a b)))
+      (min a b)
+      (gcd (min a b) (modulo (max a b) (min a b)))))
