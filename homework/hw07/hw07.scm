94581a0e47a244815346758227a1d1f136545296
tingyunaiai9 <2920825585@qq.com>
2024-08-05 14:31:43 +0800

    finish hw07
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -1,15 +1,22 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (if (zero? exp)
+      1
+      (if (even? exp)
+          (square (pow base (/ exp 2)))
+          (* base (pow base (- exp 1))))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s)
+  (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s)
+  (car (cdr (cdr s))))
