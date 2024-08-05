e5305095c1c9226a49f19096f94414fcd307de7c
tingyunaiai9 <2920825585@qq.com>
2024-08-05 21:14:25 +0800

    finish hw08
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- homework/hw08/hw08.scm
+++ homework/hw08/hw08.scm
@@ -1,7 +1,28 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    (if (null? s)
+        #t
+        (if (null? (cdr s))
+            #t
+            (if (> (car s) (car (cdr s)))
+                #f
+                (ascending? (cdr s))))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (if (null? s)
+        '()
+        (if (pred (car s))
+            (cons (car s) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (if (null? lst1)
+        lst2
+        (if (null? lst2)
+            lst1
+            (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    (if (null? s)
+        '()
+        (cons (car s) (no-repeats (my-filter (lambda (x) (not (= x (car s)))) (cdr s))))
+        ))
